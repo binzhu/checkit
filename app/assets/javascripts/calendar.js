f30b1ab818e190d16a73fc32bc048238ec3a3dfc
Bin Zhu <binzhu1070@gmail.com>
2012-07-23 23:20:01 -0400

    checkit

---
+++ app/assets/javascripts/calendar.js
@@ -0,0 +1,276 @@
+jQuery(document).ready(function() {
+	//alert("loaded!");
+	var monthNameList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	
+	var monthDayTotals = ["31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"];
+	var monthDayTotalsLeap = ["31", "29", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"];
+
+	var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	
+	
+	var weekList = new Array();
+	
+	
+	var theYear = 2012;
+	var theWeek = 1;
+	var leapYearCount = 1;
+	var leapYear = true;
+	var week52total = 1;
+	var currentMonth = 0;
+	
+	var day365 = 1;
+	var dayTotal = 366;
+	var yearDayTotal;
+	var day7 = 1;
+	var weekday1;
+	var weekday2;
+	var weekday3;
+	var weekday4;
+	var weekday5;
+	var weekday6;
+	var weekday7;
+	var total365 = 1;
+	var useLeapArray;
+	var theArray;
+	for (var loopFuel = 1; loopFuel < 100; loopFuel++) {
+		
+		if (leapYearCount == 1) {
+			// leap year
+			leapYear = true;
+			useLeapArray = true;
+			dayTotal = 367; 
+			theArray = monthDayTotalsLeap;
+			//leap();
+			leapYearCount++;
+		} else if (leapYearCount == 4) {
+			leapYearCount = 1;
+			leapYear = false;
+			useLeapArray = false;
+			dayTotal = 366; 
+			theArray = monthDayTotals;
+			//leap();
+		} else {
+			//not leap year
+			leapYear = false;
+			useLeapArray = false;
+			dayTotal = 366; 
+			theArray = monthDayTotals;
+			//leap();
+			leapYearCount++;
+		}
+		
+		/*function leap() {
+			if (leapYear == true) {
+				jQuery("#box").append("<p>leap year!" + loopFuel + "</p>");
+			} else {
+				jQuery("#box").append("<p>not leap year!" + loopFuel + "</p>");
+			}
+		}*/
+		
+		
+			for (var i=0; i<dayTotal; i++) {
+				if (day7 == 1) {
+					weekday1 = monthNameList[currentMonth] + " " + day365;
+				} else if (day7 == 2) {
+					weekday2 = monthNameList[currentMonth] + " " + day365;
+				} else if (day7 == 3) {
+					weekday3 = monthNameList[currentMonth] + " " + day365;
+				} else if (day7 == 4) {
+					weekday4 = monthNameList[currentMonth] + " " + day365;
+				} else if (day7 == 5) {
+					weekday5 = monthNameList[currentMonth] + " " + day365;
+				} else if (day7 == 6) {
+					weekday6 = monthNameList[currentMonth] + " " + day365;
+				} else if (day7 == 7) {
+					weekday7 = monthNameList[currentMonth] + " " + day365;
+				}
+				
+				if (day365 == theArray[currentMonth] ) {
+					day365 = 1;
+					if (currentMonth == 11) {
+						currentMonth = 0;
+						theYear++;
+					} else {
+						currentMonth++;
+					}
+				} else {
+					day365++;
+				}
+				
+				if(day7 == 7) {
+					day7 = 1;
+					weekList.push({
+					weekYear: theYear, 
+					weekSunday: weekday1 + ", " + theYear, 
+					weekMonday: weekday2 + ", " + theYear, 
+					weekTuesday: weekday3 + ", " + theYear, 
+					weekWednesday: weekday4 + ", " + theYear, 
+					weekThursday: weekday5 + ", " + theYear, 
+					weekFriday: weekday6 + ", " + theYear,  
+					weekSaturday: weekday7 + ", " + theYear
+					});
+				} else {
+					day7++;
+				}
+				
+				
+			} // for end
+	} //for loopfuel
+	
+	
+	
+	var current = new Date();  
+ 	var month = current.getMonth();  
+ 	var day = current.getDate();  
+ 	var year = current.getFullYear();
+ 	//var thisDay = current.getDay();
+ 	var dateString = monthNameList[month] + " " + day + ", " +  year;
+ 	
+ 	for (var c = 0; c < 100; c++) {
+ 		if (dateString == weekList[c].weekSunday) {
+ 			//alert("Sunday Match");
+ 			fillTableHead();
+ 			break;
+ 		} else if (dateString == weekList[c].weekMonday) {
+ 			//alert("Monday Match");
+ 			fillTableHead();
+ 			break;
+ 		} else if (dateString == weekList[c].weekTuesday) {
+ 			//alert("Tuesday Match");
+ 			fillTableHead();
+ 			break;
+ 		} else if (dateString == weekList[c].weekWednesday) {
+ 			//alert("Wednesday Match");
+ 			fillTableHead();
+ 			break;
+ 		} else if (dateString == weekList[c].weekThursday) {
+ 			//alert("Thursday Match");
+ 			fillTableHead();
+ 			break;
+ 		} else if (dateString == weekList[c].weekFriday) {
+ 			//alert("Friday Match");
+ 			fillTableHead();
+ 			break;
+ 		} else if (dateString == weekList[c].weekSaturday) {
+ 			//alert("Saturday Match");
+ 			fillTableHead();
+ 			break;
+ 		}
+ 	}
+ 	
+ 	function fillTableHead() {
+ 		jQuery("#days-header thead tr").append("<th>" + weekList[c].weekSunday + "</th><th>" + weekList[c].weekMonday + "</th><th>" + weekList[c].weekTuesday + "</th><th>" + weekList[c].weekWednesday + "</th><th>" + weekList[c].weekThursday + "</th><th>" + weekList[c].weekFriday + "</th><th>" + weekList[c].weekSaturday + "</th>");
+ 		jQuery(".weekBeginMonth").text(weekList[c].weekSunday);
+ 		jQuery(".weekEndMonth").text(weekList[c].weekSaturday);
+ 		jQuery('#days-header thead tr th').each(function() {
+       	 	//jQuery(this).text(jQuery(this).text().replace(', 2012', ''));
+       	 	jQuery(this).text(jQuery(this).text().replace(", " + weekList[c].weekYear + "", ""));
+    	});
+ 	}
+ 	
+ 	jQuery("#previousWeek").click(function(event) {
+ 		event.preventDefault();
+ 		//alert("clicked!");
+ 		c--;
+ 		clearTableHead();
+ 	});
+ 	
+ 	jQuery("#nextWeek").click(function(event) {
+ 		event.preventDefault();
+ 		//alert("clicked!");
+ 		c++;
+ 		clearTableHead();
+ 	});
+ 	
+ 	function clearTableHead() {
+ 		jQuery("#days-header thead tr").empty();
+ 		fillTableHead();
+ 		//stripYear();
+ 	}
+ 	
+ 	function stripYear() {
+ 		
+    }
+    
+    stripYear();
+    
+    var clickCheck = false;
+    jQuery("#friends-btn").click(function() {
+    	
+    	if (clickCheck == false) {
+    		clickCheck = true;
+    		jQuery(this).parents(".friends h2").find("span.arrow").css("background-position", "-64px -16px");
+    	} else {
+    		clickCheck = false;
+    		jQuery(this).parents(".friends h2").find("span.arrow").css("background-position", "-32px -16px");
+    	}
+    	jQuery(this).parents(".friends").find("#friends-list").slideToggle();
+    });
+    
+ 	
+ 	//alert(dateString);
+	
+	
+	
+	/*function updateText() {
+		for (var c = 0; c < 1000; c++) {
+		//jQuery("#box").append("<p>" + weekList[i].weekYear + "</p>");
+		jQuery("#box").append("<p>" + weekList[c].weekYear + "," + weekList[c].weekSunday + "," + weekList[c].weekMonday + "," + weekList[c].weekTuesday + "," + weekList[c].weekWednesday + "," + weekList[c].weekThursday + "," + weekList[c].weekFriday + "," + weekList[c].weekSaturday + "</p>");
+		}
+		
+	}*/
+	
+	// fire the text output
+	//updateText();
+	
+	var mainHeight = jQuery("#main").height();
+	var sidebarHeight = jQuery("#sidebar-left").height();
+	
+	function setHeight() {
+		//alert(mainHeight);
+		if (mainHeight > sidebarHeight) {
+			jQuery("#sidebar-left").css("height", mainHeight);
+		}
+	}
+ 
+ 	setHeight();
+ 	
+ 	
+ 	
+ 	//jQuery(".slide-box").css("margin-top", "100px");
+ 	
+ 	jQuery(".slide-box").draggable({
+		containment: "parent"
+	});
+	
+	jQuery(".slide-box").bind("drag", function() {
+		//jQuery(this).css("background-color", "pink");
+		var barOffset = jQuery(".scroll-padding").offset();
+		//alert(barOffset.top);
+		var barTop = barOffset.top;
+		var slideOffset = jQuery(this).offset();
+		var slideTop = slideOffset.top;
+		jQuery(".slide-box").css("top", slideTop-barTop);
+		
+		var slideDifference = slideTop - barTop;
+		var slidePercent = (slideDifference * 100) / 450;
+
+		var calPercent = ((1125 * slidePercent) / 100) * -1;
+		jQuery(".days").css("margin-top", calPercent);
+		jQuery("#times").css("margin-top", calPercent);  
+		jQuery(".percent").text(slidePercent);
+	});
+	
+	
+	// initial position, moves everything to 8am
+	function setSlide() {
+		// begin at 8:00am
+ 		jQuery(".days").css("margin-top", "-400px");
+ 		jQuery("#times").css("margin-top", "-400px");
+ 		var slidePosition = (32*500)/100;
+ 		jQuery(".slide-box").css("top", slidePosition);
+		
+	}
+ 	setSlide();
+ 	
+});  // ready method end 
---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,241 @@
+jQuery(document).ready(function() {
+	//alert("loaded!");
+	
+	
+	
+	var boxEndHeight = jQuery(".box").height();
+	
+	
+	jQuery(".box").draggable({
+		containment: "parent"
+	}).resizable({handles: 's'});
+	
+	
+	
+    var currentBoxHeight;
+    var theBoxHeight = jQuery(".box").height();
+ 	jQuery(".box").bind("resizestop", function(event, ui) {
+ 		//alert("stop");
+ 		currentBoxHeight = jQuery(this).height();
+ 		theBoxHeight = currentBoxHeight;
+ 		roundBoxHeight();
+ 		updateText();
+	});
+	
+	var newBoxHeight;
+	var boxTime;
+	var newBoxHeightBorder;
+	function roundBoxHeight() {
+		newBoxHeight = Math.round(currentBoxHeight / 25) * 25;
+		
+		// determine extra height for borders
+		newBoxHeightBorder = Math.round(newBoxHeight/50);
+		theBoxHeight = newBoxHeight;
+		
+		jQuery(".box").animate({
+			"height": newBoxHeight,
+			"width": "110px"
+		});
+		boxTime = Math.round(newBoxHeight/25)*.5;
+		updateText();
+		//alert(theBoxHeight);
+		updateEvent();
+	}
+	
+	var initStartTime;
+	var initPosY;
+	var initPosX;
+	var initDay;
+	var initDayNum;
+	function initBox() {
+		//alert("init");
+		//alert(eventArray[0].eventStartTime / 60);
+		initStartTime = eventArray[0].eventStartTime / 60;
+		
+		initPosY = initStartTime * 50;
+		
+		initDay = eventArray[0].eventDay;
+		//alert(initDay);
+		if (initDay == "Monday") {
+			initDayNum = 1;
+		} else if (initDay == "Tuesday") {
+			initDayNum = 2;
+		} else if (initDay == "Wednesday") {
+			initDayNum = 3;
+		} else if (initDay == "Thursday") {
+			initDayNum = 4;
+		} else if (initDay == "Friday") {
+			initDayNum = 5;
+		} else if (initDay == "Saturday") {
+			initDayNum = 6;
+		}
+		
+		//alert(initDayNum);
+		
+		if (initDay == "Sunday") {
+			initPosX = 0;
+		} else {
+		 	initPosX = initDayNum * 121;
+		}
+		
+		jQuery(".box").animate({
+			"left": initPosX,
+			"top": initPosY
+		}, "fast");
+	}
+	
+	initBox();
+	
+	var row = "<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>"
+	function buildRows() {
+		for(var g=0; g<24; g++) {
+		jQuery(".days table").append(row);
+		}
+	}
+	buildRows();
+	
+	jQuery("#cal-days table tr:odd").find("td").css("background-color", "#f5fcff");
+	
+	var boxOffset;
+	var boxTop;
+	var boxLeft;
+	
+	jQuery(".box").bind("dragstop", function(event, ui) {
+  		//alert("stop");
+  		boxOffset = jQuery(".box").offset();
+		boxTop = boxOffset.top;
+		boxLeft = boxOffset.left;
+		//alert("y is:" + boxTop + " and x is:" + boxLeft);
+  		updateText();
+  		moveBox();
+  		
+	});
+	
+	function updateText() {
+		jQuery("#sidebar").find(".boxX").text(boxLeft);
+		jQuery("#sidebar").find(".boxY").text(boxTop);
+		jQuery("#sidebar").find(".boxHeight").text(currentBoxHeight);
+		jQuery("#sidebar").find(".boxTime").text(boxTime);
+	}
+	
+	var days = jQuery(".days");
+	var daysPos = days.offset();
+	var daysPosLeft = daysPos.left;
+	var daysPosTop = daysPos.top;
+	var numX1;
+	var numX2;
+	var numY1;
+	var numY2;
+	
+	function moveBox() {
+		//roundNum = (Math.round(boxLeft / 120) * 120);
+		
+		// X values
+		numX1 = boxLeft - daysPosLeft;
+		numX2 = Math.round(numX1 / 120) * 120;
+		
+		// Y values
+		numY1 = boxTop - daysPosTop;
+		numY2 = Math.round(numY1 / 25) * 25;
+		numY2 = boxEndHeight;
+		//alert(num2);
+		//daysDiff = roundNum - daysPosLeft;
+		//alert(daysDiff);
+		jQuery(".box").animate({
+			"left": numX2,
+			"top": numY2
+		}, "fast");
+		updateEvent();
+	}
+	
+	var newAMPM;
+	var newEndAMPM;
+	var newStartTimeString;
+	var newEndTimeString;
+	function updateEvent() {
+		// new start time
+		var newStartTimeHour = Math.floor(numY2 / 50);
+		var newStartTimeHalf = numY2 / 50;
+		if (newStartTimeHour < 12) {
+			newAMPM = "am";
+		} else {
+			newAMPM = "pm";
+		}
+	
+		if (newStartTimeHalf > newStartTimeHour) {
+			if (newStartTimeHour >= 13) {
+			newStartTimeHour -= 12;
+			} else if (newStartTimeHour < 1) {
+				newStartTimeHour = 12;
+			}
+			newStartTimeString = newStartTimeHour + ":30" + newAMPM;
+		} else {
+			if (newStartTimeHour >= 13) {
+			newStartTimeHour -= 12;
+			} else if (newStartTimeHour < 1) {
+				newStartTimeHour = 12;
+			}
+			newStartTimeString = newStartTimeHour + newAMPM;
+		}
+	
+		// new end time
+		var newEndTimeHour = Math.floor((boxEndHeight + theBoxHeight)/50);
+		var newEndTimeHalf = (boxEndHeight + theBoxHeight)/50;
+		if (newEndTimeHour < 12) {
+			newEndAMPM = "am";
+		} else {
+			newEndAMPM = "pm";
+		}
+		if (newEndTimeHalf > newEndTimeHour) {
+			if (newEndTimeHour >= 13) {
+			newEndTimeHour -= 12;
+			}
+			newEndTimeString = newEndTimeHour + ":30" + newEndAMPM;
+		} else {
+			if (newEndTimeHour >= 13) {
+			newEndTimeHour -= 12;
+			}
+			newEndTimeString = newEndTimeHour + newEndAMPM;
+		}
+		
+		//alert(newEndTimeHour);
+		jQuery(".box").find(".eventStartTime").text(newStartTimeString);
+		jQuery(".box").find(".eventEndTime").text(newEndTimeString);
+	}
+
+	
+	// "times" table: the side column that holds 12:00am, 1:00am, etc.
+	
+	var timeCount = 0;
+	
+	var timeRow;
+	var ampm = "am";
+	var timeHours = 12;
+	
+	for (timeCount = 0; timeCount <24; timeCount++) {
+		if (timeCount == 12) {
+			ampm = "pm";
+			timeHours = 12;
+		}		
+
+		timeRow = "<tr><td><div class='time-padding'>" + timeHours + ":00" + ampm + "</div></td></tr>";
+		jQuery("#times").append(timeRow);
+		
+		
+		if (timeHours == 12) {
+			timeHours = 0;
+		}
+		
+		timeHours++;
+		
+		
+	}
+	
+	jQuery("#times tr:odd").find("td").css("background-color", "#f5fcff");
+	
+	
+ 
+	
+
+	
+});  // ready method end 
